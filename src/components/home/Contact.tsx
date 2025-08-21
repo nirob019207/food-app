0f77fcbe2ea3b86ce6747f4ff0a8e407d7386f06
nirob019207 <nirobhasanbd01955@gmial.com>
2025-08-21 21:57:37 +0600

    .

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -1,4 +1,4 @@
-import { Phone, Mail, MapPin, Clock } from "lucide-react"
+import { Phone, Mail, MapPin } from "lucide-react"
 
 export default function Contact() {
   return (
--- src/redux/api/deliveryApi.ts
+++ src/redux/api/deliveryApi.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { baseApi } from "@/redux/api/baseApi";
 
 export const deliveryApi = baseApi.injectEndpoints({
